c74abee31aeec0d24aaaccc3cc1f509541a1c79b
Grgrgrrr <grgrgrrr29@gmail.com>
2015-11-12 21:42:27 +0300

    Классификация с помощью svm алгоритма

--- classification.R
+++ classification.R
@@ -2,9 +2,17 @@
 library('e1071')
 library('rpart')
 
-data <- read.table("matrix.csv", header=FALSE, sep=" ")
+train <- read.table("matrix.csv", header=FALSE, sep=" ")
+test <- read.table("matrixTest.csv", header=FALSE, sep=" ")
 
-#NB <- naiveBayes(class ~ , data = data)
-#summary(NB)
+#n <- nrow(train)
 
-#prediction <- predict(NB, data)
+svmclass <- svm(train[,6714] ~ . , data = train, kernel = "linear")
+prediction <- predict(svmclass, test)
+n <- length(prediction)
+result <- vector(length = n)
+for (i in 1:n)
+{
+  result[i] = levels(factor(prediction[[i]]))
+}
+write(result, file = "predTest.csv", ncolumns = 1)
